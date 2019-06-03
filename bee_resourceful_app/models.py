c9b658de5043b98b3fb82e8d1eb7e7779c19af7a
Ronnie Gross <rnngrss1495@gmail.co>
2019-06-03 16:25:38 -0400

    trying to make heroku migrations

--- bee_resourceful_app/models.py
+++ bee_resourceful_app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=60)
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=40)
+    birthday = models.DateField(auto_now=False, auto_now_add=False)
+
+    def __str__(self):
+        return self.title
+
+class Resource(models.Model):
+    resource_name = models.CharField(max_length=255)
+    resource_number = models.IntegerField(max_length=10)
+    resource_website = models.CharField(max_length=60)
+
+    CATEGORY_CHOICES = (
+    ("FA", "Financial Assistance"),
+    ("FO", "Food"),
+    ("HO", "Housing"),
+    ("MD", "Medical & Dental"),
+    ("MH", "Mental Health"),
+    )
+
+    month = models.CharField(max_length=21,
+        choices=CATEGORY_CHOICES,
+        default="Pick a Category")
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='resources')
+
+    def __str__(self):
+        return self.name
+
--- bee_resourceful_project/urls.py
+++ bee_resourceful_project/urls.py
@@ -17,9 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.FrontendAppView.as_view()), #New URL for the index route
-    path('api/v1/', include('bee_resourceful_app.urls')),
+    path('api/v1/', include('../bee_resourceful_app.urls')),
 ]
-
