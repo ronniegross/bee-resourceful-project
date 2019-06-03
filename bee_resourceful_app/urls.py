b20794d6405c71304b2cdcd3b3789e0db66f5aad
Ronnie Gross <rnngrss1495@gmail.co>
2019-06-03 17:39:36 -0400

    created serializers, views, and urls

--- bee_resourceful_app/urls.py
+++ bee_resourceful_app/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path, include
+from rest_framework import routers
+
 from . import views
 
+router = routers.DefaultRouter()
+router.register('users', views.UserView)
+router.register('resources', views.ResourceView)
+
+
 urlpatterns = [
+    path('', include(router.urls))
 ]
--- bee_resourceful_app/views.py
+++ bee_resourceful_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+
+from .serializers import UserSerializer, ResourceSerializer
+from .models import User, Resource
+
+
+class UserView(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class ResourceView(viewsets.ModelViewSet):
+    queryset = Resource.objects.all()
+    serializer_class = ResourceSerializer
